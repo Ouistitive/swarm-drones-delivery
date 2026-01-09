6d1c5c31367de1235731514347111b00e99a0ebc
Ouistitive <ouistitive@gmail.com>
2026-01-09 12:50:01 +0100

    use autoload env and add environment variables

--- cmd/swarm-drones-delivery/main.go
+++ cmd/swarm-drones-delivery/main.go
@@ -2,28 +2,19 @@ package main
 
 import (
 	"log"
-	"os"
 	"swarm-drones-delivery/internal/constants"
 	"swarm-drones-delivery/internal/ui"
 
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load(".env")
-	if err != nil{
-		log.Fatalf("Error loading .env file: %s", err)
-	}
-
-	LAYOUT_PATH := os.Getenv("LAYOUT_PATH")
-	
 	// ebiten.SetFullscreen(true)
 	ebiten.SetWindowSize(constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT)
 	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
 	ebiten.SetWindowTitle("Swarm drones delivery simulation")
 
-	game := ui.NewGame(LAYOUT_PATH)
+	game := ui.NewGame(constants.LAYOUT_PATH)
 	game.Sim.Run()
 	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
---
+++ internal/constants/env.go
@@ -0,0 +1,35 @@
+package constants
+
+import (
+	"os"
+	"strconv"
+
+	_ "github.com/joho/godotenv/autoload"
+)
+
+// loaded from .env using autoload
+var (
+	LAYOUT_PATH			 	= os.Getenv("LAYOUT_PATH")
+	NB_AGENTS 				= envInt("NB_AGENTS", 20)
+
+	INCREMENTAL_VELOCITY	= envFloat("INCREMENTAL_VELOCITY", 0.005)
+	INCREMENTAL_DIRECTION 	= envFloat("INCREMENTAL_DIRECTION", 0.05)
+	MIN_VELOCITY 			= envFloat("MIN_VELOCITY", 0.1)
+	MAX_VELOCITY 			= envFloat("MAX_VELOCITY", 1.0)
+
+	TIC_DURATION 			= envInt("TIC_DURATION", 30)
+)
+
+func envInt(key string, def int) int {
+	if v, _ := strconv.Atoi(os.Getenv(key)); v >= 0 {
+		return v
+	}
+	return def
+}
+
+func envFloat(key string, def float64) float64 {
+	if v, _ := strconv.ParseFloat(os.Getenv(key), 64); v > 0 {
+		return v
+	}
+	return def
+}
