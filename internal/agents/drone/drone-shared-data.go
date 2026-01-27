33e49772fb78d0f50faff6e01b7506a4a212c6ee
Ouistitive <ouistitive@gmail.com>
2026-01-27 18:38:52 +0100

    ordering package missions

--- internal/agents/drone/drone-shared-data.go
+++ internal/agents/drone/drone-shared-data.go
@@ -1,10 +1,12 @@
 package drone
 
 import (
+	"sort"
+	"swarm-drones-delivery/internal/core"
+	"swarm-drones-delivery/internal/utils"
 	"swarm-drones-delivery/internal/world"
 )
 
 type DroneSharedData struct {
 	Addresses []world.DeliveryDestination
 }
-
